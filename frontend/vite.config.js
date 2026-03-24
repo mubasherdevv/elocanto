c5dcad1a2813ec48e446b6616f9d1c58742d8a53
Your Name <you@example.com>
2026-03-24 14:50:20 +0500

    Disable manual chunk splitting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
+        /*
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
@@ -34,6 +35,7 @@ export default defineConfig({
             return 'vendor';
           }
         }
+        */
       }
     }
   }
